47e232516ba808ce346a0c976d7d1d071f7b0205
이승엽 <126196812+DooDooLee@users.noreply.github.com>
2024-03-05 21:04:33 +0900

    [Bronze V] Title: A×B, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] arg){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a*b);
+    }
+}
