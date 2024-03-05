c76668ed859d0cd08514a0132fcb52e5b607071b
이승엽 <126196812+DooDooLee@users.noreply.github.com>
2024-03-05 21:22:16 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] arg){
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+
+        System.out.printf(a+"??!");
+                
+    }
+}
