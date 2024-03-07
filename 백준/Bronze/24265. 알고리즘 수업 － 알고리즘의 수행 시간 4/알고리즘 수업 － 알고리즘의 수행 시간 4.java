f2cb1235153b51d7e6bdeca41d919de49dafe73b
이승엽 <126196812+DooDooLee@users.noreply.github.com>
2024-03-07 14:32:42 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long i = sc.nextLong();
+
+        System.out.println((i)*(i-1)/2);
+        System.out.println(2);
+
+        sc.close();
+    }
+}
