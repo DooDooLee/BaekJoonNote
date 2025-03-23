1cf958dda4de80d2ba206e95eec270daf4c02de0
이승엽(Seung-Yeop Lee) <126196812+DooDooLee@users.noreply.github.com>
2025-03-23 16:58:37 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 428 ms, Memory: 41160 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        while(T-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while(st.hasMoreTokens()){
+                sb.append(new StringBuilder(st.nextToken()).reverse()).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb.toString());
+        }
+
+
+
+    }
