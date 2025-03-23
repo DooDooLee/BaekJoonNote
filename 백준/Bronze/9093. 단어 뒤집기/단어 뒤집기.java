ea6dd9d8c511bb017b585292c78107ad4f3a3352
이승엽(Seung-Yeop Lee) <126196812+DooDooLee@users.noreply.github.com>
2025-03-23 17:05:09 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 420 ms, Memory: 49244 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,23 +1,28 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
+        // BufferedReader로 입력 받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
+        // BufferedWriter로 출력 최적화
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while(T-- > 0){
+        while(T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            StringBuilder lineBuilder = new StringBuilder();  // 한 줄에 대한 StringBuilder
+
             while(st.hasMoreTokens()){
-                sb.append(new StringBuilder(st.nextToken()).reverse()).append(" ");
+                // 단어 뒤집기 후 lineBuilder에 추가
+                lineBuilder.append(new StringBuilder(st.nextToken()).reverse().toString()).append(" ");
             }
-            sb.append("\n");
+            // 한 줄 출력
+            bw.write(lineBuilder.toString() + "\n");  // 마지막 공백을 제거하고 개행 추가
         }
 
-        System.out.print(sb.toString());
-        }
-
-
-
+        // 버퍼에 있는 모든 내용 출력
+        bw.flush();
     }
+}
