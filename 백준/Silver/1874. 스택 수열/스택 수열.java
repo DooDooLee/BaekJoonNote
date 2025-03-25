3344556b653ae56d0543529067af26414e435d3a
이승엽(Seung-Yeop Lee) <126196812+DooDooLee@users.noreply.github.com>
2025-03-25 16:31:43 +0900

    [Silver II] Title: 스택 수열, Time: 292 ms, Memory: 27860 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,42 +1,36 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        Stack<Integer> st = new Stack<>();
-        
+
         int T = Integer.parseInt(br.readLine());
-        int current = 1;  // 1부터 시작
         boolean isValid = true;
+        int current = 1;
+        Stack<Integer> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < T; i++) {
-            int target = Integer.parseInt(br.readLine());
-
-            // target 값이 current보다 크다면, current부터 target까지 push
-            while (current <= target) {
-                st.push(current++);
+        for(int i =0; i<T; i++){
+            int number = Integer.parseInt(br.readLine());
+            while(current <= number){
+                st.push(current);
                 sb.append("+\n");
+                current++;
             }
-
-            // 스택의 top이 target이면 pop
-            if (!st.isEmpty() && st.peek() == target) {
-                st.pop();
-                sb.append("-\n");
-            } else {
+            if(st.pop() != number){
+                System.out.println("NO");
                 isValid = false;
                 break;
             }
+            sb.append("-\n");
+
         }
 
-        // 수열을 만들 수 없으면 "NO" 출력
-        if (isValid) {
-            System.out.print(sb);
-        } else {
-            System.out.println("NO");
+        if(isValid){
+            System.out.println(sb.toString());
         }
 
         br.close();
     }
 }
+
