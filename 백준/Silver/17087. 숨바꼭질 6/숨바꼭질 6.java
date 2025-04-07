8626b3e74d3a5133835074b1276cf40535134d77
이승엽(Seung-Yeop Lee) <126196812+DooDooLee@users.noreply.github.com>
2025-04-07 17:24:20 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 300 ms, Memory: 26260 KB -BaekjoonHub

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.java
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 import java.io.*;
 
@@ -9,46 +10,29 @@ public class Main {
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(st.nextToken()); 
-		int s = Integer.parseInt(st.nextToken()); 
-		int [] arr = new int[n];
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+		int [] gap = new int[N];
 		
-		StringTokenizer st2 = new StringTokenizer(br.readLine()); 
-		
-		arr[0] = Integer.parseInt(st2.nextToken()); 
-		int min = 0;
-		
-		for(int i = 1; i<n; i++) {
-			arr[i]= Integer.parseInt(st2.nextToken()); 
-			if(Math.abs(arr[min]-s)>Math.abs(arr[i]-s)) {
-				if(arr[i]!=s) 
-				min = i;
-				}
+		for(int i = 0; i<N; i++) {
+			gap[i] = Math.abs(Integer.parseInt(st2.nextToken())-S);
 		}
 		
-		int gap = Math.abs(s-arr[min]);
 		
+		if(N>1) {
+			int D = gcd(gap[0],gap[1]);
+			for(int i = 2; i<N; i++) {
+				D = gcd(D,gap[i]);
+		}
+			bw.write(String.valueOf(D));
 		
-		boolean isAnswer = false;
-		
-			if(n==1&&n==s) {
-				gap = Math.max(Math.abs(1000000000-s),Math.abs(s-1));
-				bw.write(String.valueOf(gap));
-			}else {
-				while (gap > 1) {
-					isAnswer = true;
-					for (int i = 0; i < n; i++) {
-						if (Math.abs(arr[i] - s) % gap != 0) {
-							isAnswer = false;
-							break;
-						}
-					}
-					if (isAnswer) break;
-					gap--;
-				}
-				bw.write(String.valueOf(gap));
-			}
-			
+		}else if(N!=S) {
+			bw.write(String.valueOf(gap[0]));
+		}else {
+			int D = Math.max(Math.abs(1000000000-S),Math.abs(S-1));
+			bw.write(String.valueOf(D));
+		}
 		
 		
 		
@@ -57,5 +41,24 @@ public class Main {
 		bw.close();		
 		br.close();	
 	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static int gcd(int a, int b) {
+		while(b != 0) {
+			int temp = b;
+			b = a%b;
+			a = temp;	
+		}
+		return a;
+	}
 }
 	
