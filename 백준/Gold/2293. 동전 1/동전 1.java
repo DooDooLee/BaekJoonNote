3dc2554a826c4fa690389f3d933db36eaabdc1a1
이승엽(Seung-Yeop Lee) <126196812+DooDooLee@users.noreply.github.com>
2025-04-14 13:19:23 +0900

    [Gold IV] Title: 동전 1, Time: 128 ms, Memory: 19056 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,42 +1,37 @@
 
 import java.util.*;
 import java.io.*;
-
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken()); // 동전 개수
-		int k = Integer.parseInt(st.nextToken()); // 목표 금액
-
-		int[] coins = new int[n];
-		for (int i = 0; i < n; i++) {
-			coins[i] = Integer.parseInt(br.readLine());
-		}
-
-		int[][] dp = new int[n + 1][k + 1];
-		dp[0][0] = 1; // 0원을 만드는 방법은 1가지 (아무 동전도 안 쓰는 것)
-
-		for (int i = 1; i <= n; i++) {
-			int coin = coins[i - 1];
-			for (int j = 0; j <= k; j++) {
-				// 이전 동전까지로 j원을 만드는 경우의 수
-				dp[i][j] = dp[i - 1][j];
-
-				// 현재 동전을 써서 j원을 만드는 경우
-				if (j - coin >= 0) {
-					dp[i][j] += dp[i][j - coin];
+		
+		int n  = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int [][] dp = new int [n+1][k+1];
+		dp[0][0] =1;
+		
+		for(int i = 1; i<=n; i++) {
+			int coin = Integer.parseInt(br.readLine());
+			for(int j =0; j<=k; j++) {
+				dp[i][j] = dp[i-1][j]; // 이전 동전까지 j 를 만드는 경우의 수 , 즉 이번동전 안쓴다고 했을때 경우의 수로 일단 초기화
+				
+				if(j>=coin) {
+					dp[i][j] += dp[i][j-coin]; //이번 동전 쓴다고 하면, 이번동전까지 고려한 상태에서  j-coin 을 만드는 경우의 수를 더해줌
+					//즉 i 번째 동전까지 고려했을때 j 를 만들수 있는 경우의 수 = 이번 동전 안 쓰는 경우 + 이번 동전 쓰는 경우
 				}
 			}
 		}
-
-		// j가 k로 나눈 나머지가 0인 경우만 최종 출력 (여기선 딱 k원을 만드는 경우)
-		bw.write(dp[n][k] + "\n");
+		
+		bw.write(String.valueOf(dp[n][k]));
+			
 		bw.flush();
 		bw.close();
-		br.close();
+		br.close();		
+		
 	}
 }
