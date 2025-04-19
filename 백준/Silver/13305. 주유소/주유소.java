f541b494ae5282979e458ac04ed38dbc3bdf0141
이승엽(Seung-Yeop Lee) <126196812+DooDooLee@users.noreply.github.com>
2025-04-19 20:46:46 +0900

    [Silver III] Title: 주유소, Time: 368 ms, Memory: 35444 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,37 +1,42 @@
 
 import java.util.*;
 import java.io.*;
-public class Main {
+
+public class Main{
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n  = Integer.parseInt(br.readLine());
 		
-		int [] node = new int [n+1];  //0~n  (1~n 만 쓸거임)
- 		int [] link = new  int [n];  //0~n-1 (1~n-1) 
+		int [] node = new int [n+1];  // 기름값
+ 		int [] link = new  int [n];   // 거리
+
  		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i =1; i<n; i++) {
-			
+		for(int i = 1; i < n; i++) {
 			link[i] = Integer.parseInt(st.nextToken());
 		}
-		StringTokenizer st1= new StringTokenizer(br.readLine());
-		for(int i =1; i<=n; i++) {
-			
-			node[i] = Integer.parseInt(st1.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= n; i++) {
+			node[i] = Integer.parseInt(st.nextToken());
 		}
-		int i =1;
-		int sum=0;
-		while(i <n) {
-			sum =sum+(node[i]*link[i]);
-			int j = i+1;
-			while(node[i]<node[j]&&j<n) {
-				sum = sum+(node[i]*link[j]);
-				j++;
-			} 
-			i=j;
+
+		int i = 1;
+		long sum = 0;
+
+		while(i < n) {
+			long cost = node[i];
+			int j = i;
+			long distSum = link[j];
+
+			while(++j < n && node[j] > cost) {
+				distSum += link[j];
+			}
+
+			sum += cost * distSum;
+			i = j;
 		}
-		
+
 		System.out.println(sum);
 		br.close();		
-		
 	}
 }
