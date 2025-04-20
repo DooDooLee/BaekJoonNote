881f59b79adc2968dbab8396da2687a1b64e448d
이승엽(Seung-Yeop Lee) <126196812+DooDooLee@users.noreply.github.com>
2025-04-20 11:47:15 +0900

    [Gold V] Title: 회의실 배정, Time: 456 ms, Memory: 43992 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,39 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] time = new int[N][2];
-        StringTokenizer st;
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            time[i][0] = Integer.parseInt(st.nextToken());
-            time[i][1] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        int [][] a = new int[n][2];
+        
+        for(int i=0; i<n; i++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	a[i][0] = Integer.parseInt(st.nextToken());
+        	a[i][1] = Integer.parseInt(st.nextToken());
         }
-
-        Arrays.sort(time, (o1, o2) -> {
-            if(o1[1] == o2[1]) {
-                return o1[0] - o2[0];
-            }
-            return o1[1] - o2[1];
+        
+        Arrays.sort(a,new Comparator<int[]>() {
+        	@Override
+        	public int compare(int[] o1, int[] o2) {
+        		if(o1[1]==o2[1]) {
+        			return o1[0]-o2[0];
+        		}else {
+        			return o1[1]-o2[1];
+        		}
+        	}
+        	
         });
-
-        int cnt = 0;
+        
+        
+        int count =0;
         int end = 0;
-
-        for (int i = 0; i < N; i++) {
-            if (end <= time[i][0]) {
-                end = time[i][1];
-                cnt++;
-            }
+        for(int i=0; i<n; i++) {
+        	if(a[i][0]>=end) {
+        		count++;
+        		end = a[i][1];
+        	}
         }
-
-        System.out.println(cnt);
+        
+        System.out.println(count);        
+        br.close();
     }
 }
