49efc5833204a8c1e5cac3d13b4fd2d43b3b92dc
이승엽(Seung-Yeop Lee) <126196812+DooDooLee@users.noreply.github.com>
2025-04-30 22:56:24 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 624 ms, Memory: 117896 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,51 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static ArrayList<Integer> al;
+	public static void main(String[] args)throws IOException {
+	al = new ArrayList<>();
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int n = Integer.parseInt(br.readLine());
+	StringTokenizer st = new StringTokenizer(br.readLine());
+
+	al.add(Integer.parseInt(st.nextToken()));
+	
+	for(int i = 1; i<n; i++) {
+		int a = Integer.parseInt(st.nextToken());
+		if(al.get(al.size()-1)<a) {
+			al.add(a);
+		}else {
+			int idx = bin(a);
+			al.set(idx, a);
+		}
+	}
+	
+	
+	System.out.println(al.size());
+	
+	
+	
+	br.close();
+	
+	}
+	static int bin(int a) {
+		int left = 0;
+		int right = al.size();
+		
+		while(right>left) {
+			int  mid = (left+right)/2;
+			if(a>al.get(mid)) {
+				left = mid+1;
+			}else if(al.get(mid) == a){
+				return mid;
+			}else {
+				right = mid;
+			}
+		}
+		
+		
+		return left;
+	}
+}
