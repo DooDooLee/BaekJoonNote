298af87ae8e0359d2ee2e79a65572b600615a736
이승엽(Seung-Yeop Lee) <126196812+DooDooLee@users.noreply.github.com>
2025-05-07 02:20:21 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		long dp []= new long [n+1];
+		dp[1] = 1;
+		if(n>=2) {
+			dp[2] = 3;
+			if(n>=3) {
+				dp[3] = 5;
+			}
+		}
+		
+		
+		
+		for(int i=4; i<=n; i++) {
+			dp[i] = (dp[i-1]+ 2*dp[i-2])%10007;  
+		}
+		
+		
+		System.out.println(dp[n]);
+		
+		
+	}
+}
