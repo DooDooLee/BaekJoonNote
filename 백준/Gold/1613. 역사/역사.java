7eb654ca94df21bcfa3302c9ba53b681be2dceec
이승엽(Seung-Yeop Lee) <126196812+DooDooLee@users.noreply.github.com>
2025-05-13 17:27:03 +0900

    [Gold III] Title: 역사, Time: 848 ms, Memory: 46100 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.java
+++ 백준/Gold/1613. 역사/역사.java
@@ -1,61 +1,64 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
+	
+	public static void main(String[] args)throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int [][] arr = new int [n+1][n+1];
+		for(int i =0; i<n+1; i++) {
+			Arrays.fill(arr[i], 9999);
+		}
+		
+		for(int i =0; i<k; i++) {
+			StringTokenizer st2 = new StringTokenizer(br.readLine());
+			int d = Integer.parseInt(st2.nextToken());
+			int e = Integer.parseInt(st2.nextToken());
+			arr[d][e] = 1;
 
-    public static void main(String[] args)throws IOException {
+		}
+		int s = Integer.parseInt(br.readLine());
+		
+		
+		for(int m=1; m<=n; m++) {
+			for(int i =1; i<=n; i++) {
+				for(int j=1; j<=n; j++) {
+					int way = arr[i][m] + arr[m][j];
+					if(way<arr[i][j]) {
+						arr[i][j]=way;
+					}
+				}
+			}
+		}
+		
+		
+		
+		
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        int[][] arr = new int[n+1][n+1];
-
-        // 무한대로 초기화
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(arr[i], Integer.MAX_VALUE);
-        }
-
-        // 자기 자신은 0으로 초기화
-        for (int i = 1; i <= n; i++) {
-            arr[i][i] = 0;
-        }
-
-        // 전후 관계 입력
-        for (int i = 0; i < k; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            arr[from][to] = 1; // 단방향 그래프
-        }
-
-        // 플로이드-워셜
-        for (int m = 1; m <= n; m++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (arr[i][m] != Integer.MAX_VALUE && arr[m][j] != Integer.MAX_VALUE) {
-                        arr[i][j] = Math.min(arr[i][j], arr[i][m] + arr[m][j]);
-                    }
-                }
-            }
-        }
-
-        // 쿼리 처리
-        int s = Integer.parseInt(br.readLine());
-        for (int i = 0; i < s; i++) {
-            st = new StringTokenizer(br.readLine());
-            int d = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-
-            if (arr[d][e] != Integer.MAX_VALUE) {
-                System.out.println(-1); // d가 e보다 먼저 발생
-            } else if (arr[e][d] != Integer.MAX_VALUE) {
-                System.out.println(1); // e가 d보다 먼저 발생
-            } else {
-                System.out.println(0); // 알 수 없음
-            }
-        }
-    }
-}
+		for(int i =0; i<s; i++) {
+			StringTokenizer st3 = new StringTokenizer(br.readLine());
+			int d = Integer.parseInt(st3.nextToken());
+			int e = Integer.parseInt(st3.nextToken());
+			
+			if(arr[d][e]>=9999 && arr[e][d]>=9999) {
+				System.out.println(0);
+			}else if(arr[d][e] >= 9999) {
+				System.out.println(1);
+			}else {
+				System.out.println(-1);
+			}
+		}
+		
+		
+	}
+	
+}    
+    
