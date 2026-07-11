08a7e3c5d9a7c62554dc3daaae0f71d5cc5c52b6
이승엽 <126196812+DooDooLee@users.noreply.github.com>
2026-07-12 02:17:05 +0900

    [level 1] Title: 달리기 경주, Time: 208.54 ms, Memory: 368 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> rankMap = new HashMap<>();
+        
+        for (int i = 0; i < players.length; i++) {
+            rankMap.put(players[i], i);
+        }
+        
+        for (String calling : callings) {
+            int currentRank = rankMap.get(calling);
+            String frontPlayer = players[currentRank - 1];
+            
+            players[currentRank - 1] = calling;
+            players[currentRank] = frontPlayer;
+            
+            rankMap.put(calling, currentRank - 1);
+            rankMap.put(frontPlayer, currentRank);
+        }
+        
+        return players;
+    }
+}
